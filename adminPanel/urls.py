9a3bb8749b82fddfbfd995678229365525ba6e8d
Ruben14f <mansilla8090@gmail.com>
2025-05-11 23:38:33 -0400

    product_status_filtro_producto

--- adminPanel/urls.py
+++ adminPanel/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('eliminar-product/<int:id>', eliminar_producto, name='eliminar_product_admin'),
     path('list-ordenes', listado_ordenes, name="list_ordenes_admin"),
     path('filter-orden', orden_search, name='buscar_orden'),
+    path('filter-sku', sku_search, name='buscar_sku'),
+    path('filter-marca', marca_search, name='buscar_marca'),
     # path('filter-status', estado_search, name='buscar_estado'),
-    
 ]
